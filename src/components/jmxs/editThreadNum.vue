4b1de0ce31ee636fa7b7a64a27f9d73fbd26196c
chenlei <chenlei@haixue.com>
2020-10-27 22:16:13 +0800

    添加修改线程组属性的对话框

--- src/components/jmxs/editThreadNum.vue
+++ src/components/jmxs/editThreadNum.vue
@@ -2,7 +2,7 @@
   <span style="margin-left: 10px">
     <el-button type="info" @click="initForm" size="small" class="myicon" icon="el-icon-tickets" circle/>
     <el-dialog title="修改线程属性" :visible.sync="eidtThreadNumVisible" width="25%">
-      <el-form :model="threadNumFormData" status-icon size="small" label-width="80px">
+      <el-form :model="threadNumFormData" status-icon size="small" label-width="80px" :rules="editThreadNumFormRules">
         <el-form-item label="线程数" prop="numThreads">
           <el-input v-model="threadNumFormData.numThreads" placeholder="线程数" size="small"/>
         </el-form-item>
@@ -10,13 +10,20 @@
           <el-input v-model="threadNumFormData.rampTime" placeholder="Ramp-Up时间（秒）" size="small"/>
         </el-form-item>
         <el-form-item label="循环次数" prop="loops">
-          <el-input v-model="threadNumFormData.loops" placeholder="循环次数" size="small"/>
+          <el-input v-model="threadNumFormData.loops" placeholder="循环次数，-1为永久循环" size="small"/>
         </el-form-item>
-        <el-form-item label="调度器" prop="scheduler">
-          <el-input v-model="threadNumFormData.scheduler" placeholder="使用调度器" size="small"/>
+        <el-form-item label="调度器">
+          <el-select v-model="threadNumFormData.scheduler" size="small" style="width:230px">
+            <el-option
+              v-for="item in threadNumOptions"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
         </el-form-item>
         <el-form-item label="持续时间" prop="duration">
-          <el-input v-model="threadNumFormData.duration" placeholder="持续时间" size="small"/>
+          <el-input v-model="threadNumFormData.duration" placeholder="持续时间" size="small" :disabled="threadNumFormData.scheduler==='false'"/>
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
@@ -32,6 +39,16 @@ export default {
   data() {
     return {
       eidtThreadNumVisible: false,
+      threadNumOptions: [
+        {
+          value: 'true',
+          label: '使用调度器'
+        },
+        {
+          value: 'false',
+          label: '不使用调度器'
+        }
+      ],
       threadNumFormData: {
         jmxId: '',
         numThreads: '',
@@ -39,6 +56,18 @@ export default {
         loops: '',
         scheduler: '',
         duration: ''
+      },
+      // 校验必填参数
+      editThreadNumFormRules: {
+        numThreads: [
+          { required: true, message: '请输入线程数', trigger: 'blur' }
+        ],
+        rampTime: [
+          { required: true, message: '请输入rampTime时间', trigger: 'blur' }
+        ],
+        loops: [
+          { required: true, message: '请输入循环次数，-1为永久循环', trigger: 'blur' }
+        ]
       }
     }
   },
